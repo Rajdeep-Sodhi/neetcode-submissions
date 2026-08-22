597eb7f721893b09dc22af8b6fc989f69419a4e6
Rajdeep-Sodhi <143214394+Rajdeep-Sodhi@users.noreply.github.com>
2026-08-22 10:56:27 -0700

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        /*
+        keep a non increasing monotic stack of the element's index value 
+        iternate through list
+            while top of stack if not greater than current element
+                pop
+                store at pop (this holds an index value), current index - pop
+            add to stack once iternation is done
+        for every index remaining in the stack, place a zero
+        */
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] output = new int[temperatures.length];
+
+        for(int i = 0; i < temperatures.length; i++){
+            while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
+                int pop = stack.pop();
+                output[pop] = i - pop;
+            }
+            stack.push(i);
+        }
+        return output;
+    }
+}
